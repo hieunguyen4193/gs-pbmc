c061ce2b5331390a0d770ab54f53b815c521625a
hieunguyen4193 <work@hieunguyen.de>
2025-01-29 14:50:08 +0100

    update: filter cov file by bed files

---
+++ filter_cov_file_by_bed_files.sh
@@ -0,0 +1,34 @@
+export PATH=/home/hieunguyen/bedtools2/bin:$PATH;
+
+path_to_bed_files="/media/hieunguyen/GSHD_HN01/storage/PBMC/bed_files";
+
+vi_bedfile=${path_to_bed_files}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_plus_gapfill_SpikeIn_modified.bed;
+truong_bedfile=${path_to_bed_files}/merged_probe_file_shareable_Methyl_Genesolutions_CustomMethyl_MTE-95699147_hg19_Rev1_ProbeShift_LowStringency_240712165101_modified.bed;
+
+maindir="/media/hieunguyen/GSHD_HN01/storage/PBMC";
+
+##### new batch data, 20250110, Truong
+batch_name="20250110"
+path_to_raw_cov=${maindir}/${batch_name}/raw_cov
+new_batch_name=${batch_name}_filtered_bed
+
+mkdir -p ${maindir}/${new_batch_name}/raw_cov;
+
+all_cov_files=$(ls ${path_to_raw_cov}/*.cov);
+
+for file in ${all_cov_files};do \
+echo -e "Filtering file " ${file}; 
+bedtools intersect -a ${file} -b ${truong_bedfile} -wa -wb > ${maindir}/${new_batch_name}/raw_cov/$(basename ${file});
+done
+
+##### new batch data, 20250110, Vi
+for batch_name in 20250117_Vi_Breast 20250117_Vi_Liver 20250117_Vi_Rectum 20250117_Vi_CRC 20250117_Vi_Lung 20250117_Vi_Stomach; do \
+    path_to_raw_cov=${maindir}/${batch_name}/raw_cov;
+    new_batch_name=${batch_name}_filtered_bed;
+    mkdir -p ${maindir}/${new_batch_name}/raw_cov;
+    all_cov_files=$(ls ${path_to_raw_cov}/*.cov);
+    for file in ${all_cov_files};do \
+    echo -e "Filtering file " ${file}; 
+    bedtools intersect -a ${file} -b ${vi_bedfile} -wa -wb > ${maindir}/${new_batch_name}/raw_cov/$(basename ${file});
+done;done
+
--- modify_bed_files.sh
+++ modify_bed_files.sh
@@ -1,5 +1,13 @@
+export PATH=/home/hieunguyen/bedtools2/bin:$PATH;
+
 path_to_bed_files="/media/hieunguyen/GSHD_HN01/storage/PBMC/bed_files";
 
 vi_bedfile=${path_to_bed_files}/covered_targets_Twist_Methylome_hg19_annotated_collapsed_plus_gapfill_SpikeIn.bed;
-truong_bedfile=$(path_to_bed_files)/merged_probe_file_shareable_Methyl_Genesolutions_CustomMethyl_MTE-95699147_hg19_Rev1_ProbeShift_LowStringency_240712165101.bed;
+truong_bedfile=${path_to_bed_files}/merged_probe_file_shareable_Methyl_Genesolutions_CustomMethyl_MTE-95699147_hg19_Rev1_ProbeShift_LowStringency_240712165101.bed;
+
+##### replace chr in chromosome name
+
+echo -e "replacing chr in chromosome name in bed files ...."
+awk 'BEGIN{OFS="\t"} {gsub(/^chr/, "", $1); print}' "$vi_bedfile" | cut -f1,2,3 > "${vi_bedfile%.bed}_modified.bed"
+awk 'BEGIN{OFS="\t"} {gsub(/^chr/, "", $1); print}' "$truong_bedfile" | cut -f1,2,3 > "${truong_bedfile%.bed}_modified.bed"
 
