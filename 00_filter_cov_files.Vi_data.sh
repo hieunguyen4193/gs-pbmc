ece45f702a06da2dcf32b83e1b1eb7575ef401bd
hieunguyen4193 <work@hieunguyen.de>
2025-02-02 18:21:04 +0100

    update

---
+++ 00_filter_cov_files.Vi_data.sh
@@ -0,0 +1,22 @@
+outdir="/media/hieunguyen/GSHD_HN01/storage/PBMC/20250117_Vi_data";
+
+# data_version="20240513";
+# data_version="20240601"
+# data_version="20240617";
+# data_version="20240911";
+# data_version="20241010_CRC"
+# data_version="20241010_Breast"
+
+data_group=$1;
+data_version=$2
+
+mkdir -p ${outdir}/${data_group}/${data_version}/filtered_5reads_cov;
+mkdir -p ${outdir}/${data_group}/${data_version}/filtered_3reads_cov;
+mkdir -p ${outdir}/${data_group}/${data_version}/filtered_10reads_cov;
+files=$(ls ${outdir}/${data_group}/${data_version}/raw_cov/*.cov | xargs -n 1 basename);
+
+for file in $files;do filename=${file%.cov*} && \
+echo -e "working on sample " $filename "\n" && \
+awk -F'\t' '$5 + $6 >= 5' ${outdir}/${data_group}/${data_version}/raw_cov/${filename}.cov > ${outdir}/${data_group}/${data_version}/filtered_5reads_cov/${filename}.filtered.cov && \
+awk -F'\t' '$5 + $6 >= 3' ${outdir}/${data_group}/${data_version}/raw_cov/${filename}.cov > ${outdir}/${data_group}/${data_version}/filtered_3reads_cov/${filename}.filtered.cov && \
+awk -F'\t' '$5 + $6 >= 10' ${outdir}/${data_group}/${data_version}/raw_cov/${filename}.cov > ${outdir}/${data_group}/${data_version}/filtered_10reads_cov/${filename}.filtered.cov;done
---
+++ run_00_filter_cov_files_Vi_data.sh
@@ -0,0 +1,6 @@
+for data_group in 20250117_Vi_Breast 20250117_Vi_Liver 20250117_Vi_Rectum 20250117_Vi_CRC 20250117_Vi_Lung 20250117_Vi_Stomach;do \
+    for data_version in {1..22};do \
+    echo -e "working on data gorup  " ${data_group} " at chrom " ${data_version};
+        bash 00_filter_cov_files.Vi_data.sh ${data_group} ${data_group}_chr${data_version};
+    done;
+done;
